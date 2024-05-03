6c390ffb1a35433a863d8c4ca9b591abce3d9a33
sarojadevi santhanakrishnan <mail2lotchani@gmail.com>
2024-05-03 12:54:34 +0200

    About us page is completed and content also created

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -1,18 +1,65 @@
 <x-mainlayout>
-    <x-nav-link-us>
-        
-         
-</x-nav-link-us>
-<!-- creating a about us page with two div one for heading and other for content --->
-         <div class="flex-col  mt-5 mx-40 bg-white shadow-md w-[82%] gap-4 rounded-xl">
-            <div class="mt-3 mx-14 bg-white shadow-md w-[90%] gap-4  rounded-sm">
-              About us
+  
+         <div class="flex-col">
+          <!-- first div for background image and link to home page -->
+         <div class="relative" >
+          <img src="{{ url('images/img5.avif') }}" alt="" class="bg-cover w-full h-[600px]">
+          <a href="{{ route('welcome') }}" class="font-bold text-white text-center absolute right-3.5 top-1.5 border-none w-[100px] h-[30px] bg-stone-500 rounded-xl ">Home</a>
+          <p class="font-bold text-white text-5xl absolute top-[40%] right-[45%]"><i>Lux Rent</i></p>
+          <p class="font-bold text-white text-2xl absolute top-[55%] right-[35%]"><i>Start your driving experience with us! To enjoy your Trip!</i></p>
+         </div>
+         <!-- second div for about us text which contains all the text about the about us -->
+         <div class="w-[60%] mx-auto mt-12">
+            <div class="font-bold text-6xl float-left mx-12"><i>About us</i></div>
+            <div class="font-medium">When traveling or even for special events, selecting a reliable car service provider can be of the essence. Transportation is such an important part of the overall experience. Whether getting transportation to or from the airport or ordering a luxurious car/limousine for a concert or sports event, <span class="text-1xl font-bold"><i>lux rent</i></span>  is ready to serve you. 
 
+<span class="text-1xl font-bold"> <i>lux rent</i></span> is an experienced car service provider located in <span class="font-bold text-1xl"><i>Luxembourg</i></span>. With a variety of different services, we are dedicated to giving customers an unforgettable experience. We want to ensure our guests make memories that will last for a lifetime. 
+
+We are thrilled to offer our car services to you no matter the occasion or need. Our goal is to go above and beyond for each of our clients. </div>
+
+
+</div>
+         <!-- third div to define our why us text  -->
+            <div class="w-[60%] mx-auto mt-12">
+              <div class=" grid grid-cols-3 gap-4 text-white">
+                   <div class="flex-col  p-2 gap-2 justify-center items-center border-none rounded-xl bg-stone-400">
+                           <div class="font-bold text-5xl "><i> Why Us</i></div>
+                           <div class="font-medium ">With numerous car service providers at your disposal, here are a few reasons to select us! 
+                           </div>
+                    </div>
+                     <div class="flex-col p-2 gap-2 col-span-2  justify-center items-center border-none rounded-xl bg-stone-500 " >
+                         <div class="font-bold tex-3xl"><i>Years of experience</i></div>
+                         <div class="font-medium">Our rental service company have over <span class="font-bold text-1xl"><i>15 years</i></span>  of experience. Most of our cars are new and well maintained. We only hire the most qualified and top percentage of maintenance service company in the industry to maintain our vehicles.  </div>
+          
+                      
+                          </div>
+                      
+                      <div class="flex-col p-2 gap-2 col-span-3  text-white justify-center items-center border-none rounded-xl bg-stone-600 " >
+                            <div class="font-bold tex-3xl"><i>Safety is our priority</i></div>
+                            <div class="font-medium">Our maintenance team is 100% committed to providing clients with the safest and most secure driving experience with highly maintained car. <span class="text-1xl font-bold"><i>Lux rent</i></span> upholds strict safety protocol, including fleet tracking and GPS usage.
+Right from the beginning of their purchase, all vehicles are periodically maintained on regular basis.   </div>
+                       </div>
+                       <div class="flex-col p-2 gap-2  text-white justify-center items-center border-none rounded-xl bg-stone-500 " >
+                            <div class="font-bold tex-3xl"><i>Timely service</i></div>
+                            <div class="font-medium"> We provide a timely service to our clients and we keep our promises by delivering the car on time. </div>
+                       </div>
+                       <div class="flex-col p-2 gap-2 col-span-2  text-white justify-center items-center border-none rounded-xl bg-stone-400 " >
+                            <div class="font-bold tex-3xl"><i>A Formula That Works </i></div>
+                            <div class="font-medium"><span class="text-1xl font-bold"><i>Lux Rent</i></span> is highly respected in the <span center="text-1xl font-bold"><i>Luxembourg</i></span> community. We have built a strong reputation and are trusted by our clients. This is something of great importance and pride to us. We do not take our clients’ trust lightly. 
+                            <span class="text-1xl font-bold"><i>lux rent</i></span> is delighted to have many happy customers who have publicly recommended our services through reviews and referrals. A few testimonials will be shared below.  </div>
+                       </div>
+                      
+
+               </div>
+                      
+                 
             </div>
-            <div class="mt-5 mx-14 bg-white shadow-md w-[90%] gap-4 rounded-sm">             
-    
-                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda, asperiores voluptates perspiciatis eum possimus provident, nam dignissimos maxime praesentium repudiandae accusantium consequuntur velit eius, commodi facilis. Autem voluptates vero eligendi!
-                </div>
-         </div> 
+
+
+            
+
+
+         </div>
+       
 
 </x-mainlayout>
