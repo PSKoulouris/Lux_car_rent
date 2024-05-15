ff23bf47b272d427d20242685b1b80962e3a9ee8
Stefan Flavian Hritcu <s.f.hritcu@gmail.com>
2024-05-15 09:36:13 +0200

    Changes in the user table, user model, registrationContoller, created 3 new components for DL, ID, proof, deleting the customer table beacause we create new columns in the user table, change the foreign key from booking tbl to user tbl, fixing saving 2 images in the same time in the DB

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'driver_licence',
+        'identification_data_proof',
+        'address',
     ];
 
     /**
