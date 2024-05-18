50af9b7a1bfd6268c0bd9c9342e5bd93fd401746
sarojadevi santhanakrishnan <mail2lotchani@gmail.com>
2024-05-18 11:49:10 +0200

    changes made in car view to make it feel darker color and changed the booking form to have the amount

--- public/js/app.js
+++ public/js/app.js
@@ -2,14 +2,14 @@ console.log("its working");
 
 // javascript for search form in main page
 // created a javascript to set the min attribute value to today date
-document.getElementById("start-date").addEventListener('click', function() {
-    let data=document.getElementById("start-date");
+document.getElementById("startdate").addEventListener('click', function() {
+    let data=document.getElementById("startdate");
     let newdate=new Date().toISOString().split("T")[0]; 
     data.setAttribute('min',newdate);
  });
  // created a javascript to set the min attribute value to today date
- document.getElementById("end-date").addEventListener('click', function() {
-    let dataend=document.getElementById("end-date");
+ document.getElementById("enddate").addEventListener('click', function() {
+    let dataend=document.getElementById("enddate");
     let newenddate=new Date().toISOString().split("T")[0]; 
     dataend.setAttribute('min',newenddate);
  });
@@ -21,12 +21,51 @@ document.getElementById("start-date").addEventListener('click', function() {
 //    let thankYouInfo=document.getElementById("contact-thankyou");
 //    thankYouInfo.style.display=block;
 //  })
+document.getElementById("startdate").addEventListener('change', function() {
+   let data=document.getElementById("startdate").value;
+   // let newdate=new Date().toISOString().split("T")[0]; 
+   // data.setAttribute('min',newdate);
+   console.log(data);
+});
+document.getElementById("enddate").addEventListener('change', function() {
+   let data=document.getElementById("enddate").value;
+   // let newdate=new Date().toISOString().split("T")[0]; 
+   // data.setAttribute('min',newdate);
+   console.log(data);
+});
+
+document.getElementById("quote").addEventListener('click',function(){
+   let startdate=document.getElementById("startdate").value;
+   let enddate=document.getElementById("enddate").value;
+   let rate=document.getElementById("dailyrate").value;
+   let form=document.getElementById("bookingform");
+   const dayone=new Date(startdate);
+   const daytwo=new Date(enddate);
+   const time=Math.abs(daytwo-dayone);
+   const days=Math.ceil(time/(1000*60*60*24));
+   console.log(days);
+   const quote=rate*days;
+   console.log(quote);
+   // let result=document.createElement("p");
+   // result.textContent=
+   // form.appendChild(result);
+
+   
+   
+    document.getElementById("output").innerHTML=quote;
+  
+   let rate1=document.getElementById("amount").setAttribute('value',quote);
+   console.log(rate1);
+})
 
 
    function contactfunction(){
+      // e.preventdefault();
     document.getElementById("contact-heading").style.display="none";
     document.getElementById("contact-info").style.display="none";
     document.getElementById("contact-thankyou").style.display="block";
      }
+     
  
 
+  
--- resources/views/components/car-view.blade.php
+++ resources/views/components/car-view.blade.php
@@ -1,7 +1,7 @@
 @if(isset($id))
     <a href="/carDetails/{{$id}}">
  @endif
-<div class="w-[350px] h-[400px] flex flex-col justify-center items-center shadow-md bg-gray-400 mx-auto rounded-xl" >
+<div class="w-[350px] h-[400px] flex flex-col justify-center items-center shadow-md bg-[#2D3B4E] mx-auto rounded-xl" >
     <div class="rounded-xl w-[90%] h-[220px] mx-auto bg-gray-300  flex items-center justify-center">
           <!-- <img src="images/dark1.webp" alt="" class="object-cover  w-[150px] h-[150px]"> -->
           <!-- <div class="w-[240px] h-[150px] bg-cover">{{ $image }} </div> -->
@@ -9,11 +9,11 @@
     </div>
     <div class="flex flex-col">
         {{-- passing the arguments here for the car info/view --}}
-        <p class="bg-gray-200 w-[300px] font-bold text-center mt-1 rounded-lg">{{ $nameCar }}</p>
+        <p class=" w-[300px] font-bold text-center mt-1 rounded-lg text-2xl text-orange-500">{{ $nameCar }}</p>
         <p class="bg-gray-200 w-[300px]font-bold text-center mt-1 rounded-lg">{{ $model }}</p>
         <div class="flex gap-4 w-[300px]">
-            <p class="bg-gray-200 w-[150px] font-bold text-center mt-1 rounded-lg">{{ $seat}}</p>
-            <p class="bg-gray-200 w-[150px] font-bold text-center mt-1 rounded-lg">{{ $door}}</p>
+            <p class="bg-gray-200 w-[150px] font-bold text-center mt-1 rounded-lg ">{{ $seat}} Seats</p>
+            <p class="bg-gray-200 w-[150px] font-bold text-center mt-1 rounded-lg ">{{ $door}} Doors</p>
         </div>  
         <div class="flex justify-end">
             <p class="font-medium bg-gray-200 w-[300px] text-center mt-1 rounded-lg">Best price for a week <span class="font-bold">&#8364;{{ $price }}</span> </p>
