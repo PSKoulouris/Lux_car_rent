91aeae699d21c8ee9ec383a3e7fc9ec69b077bcc
sarojadevi santhanakrishnan <mail2lotchani@gmail.com>
2024-05-19 13:11:10 +0200

    last and final changes for the project changed about us and contact page  changed as per other pages format so happy to finish the project

--- app/Http/Controllers/CarsController.php
+++ app/Http/Controllers/CarsController.php
@@ -60,7 +60,7 @@ class CarsController extends Controller
     public function showapiCars () {
         $listCars = Cars::join('link_cars_types', 'link_cars_types.car_id', '=', 'cars.id')
         ->join('cars_types', 'link_cars_types.car_type_id', '=', 'cars_types.id')
-        ->select('cars.image','cars.id','cars.name', 'cars.model', 'cars.weekly_rate','cars_types.nbr_places','cars_types.nbr_doors')->get();
+        ->select('cars.image','cars.id','cars.name', 'cars.model', 'cars.weekly_rate','cars.daily_rate','cars_types.nbr_places','cars_types.nbr_doors')->get();
         Log::info($listCars);
         return response()->json($listCars);
     }
